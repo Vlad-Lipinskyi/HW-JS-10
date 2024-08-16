f8784387cd07743e679af79a426dccd94c54dcf1
hookly <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-08-16 18:53:58 +0300

    ііі

--- index.js
+++ index.js
@@ -9,3 +9,60 @@ const showMessage = (callback) => {
 
 showMessage(messageHello);
 
+
+// 2 Завдання
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const peopleNumber = prompt("Вгадайте число від 1 до 100:");
+
+if (peopleNumber == randomNumber) {
+    alert("Вітаємо! Ви вгадали правильне число");
+} else {
+    alert(`Нажаль, ви не вгадали. Правильне число було: ${randomNumber}`);
+}
+
+
+// 3 Завдання
+
+let DataInput = 1;
+
+function getUserInput() {
+    const userInput = prompt("Введіть будь-які дані:");
+    
+    if (userInput === null) {
+        alert("Ви скасували введення.");
+
+        return;
+    }
+
+    alert(`Ви ввели дані ${DataInput} разів.`);
+
+    DataInput = DataInput + 1;
+
+    getUserInput();
+}
+
+getUserInput();
+
+
+// 4 Завдання
+
+const numbers = [1, 2, 3, 4, 5];
+
+function CallbackArray(array, callback) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+      result.push(callback(array[i]));
+  }
+  return result;
+}
+
+const multiplyNumbers = CallbackArray(numbers, function(num) {
+  return num * num;
+});
+
+console.log(multiplyNumbers);
+
+
+// 5 Завдання
+
