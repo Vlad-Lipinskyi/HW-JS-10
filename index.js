cec6f3119764676e6a141319a621f1067d6fd43e
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-08-20 15:34:23 +0300

    sss

--- index.js
+++ index.js
@@ -9,41 +9,38 @@ const showMessage = (callback) => {
 
 showMessage(messageHello);
 
-
 // 2 Завдання
 
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 const peopleNumber = prompt("Вгадайте число від 1 до 100:");
 
 if (peopleNumber == randomNumber) {
-    alert("Вітаємо! Ви вгадали правильне число");
+  alert("Вітаємо! Ви вгадали правильне число");
 } else {
-    alert(`Нажаль, ви не вгадали. Правильне число було: ${randomNumber}`);
+  alert(`Нажаль, ви не вгадали. Правильне число було: ${randomNumber}`);
 }
-
 
 // 3 Завдання
 
 let DataInput = 1;
 
 function getUserInput() {
-    const userInput = prompt("Введіть будь-які дані:");
-    
-    if (userInput === null) {
-        alert("Ви скасували введення.");
+  const userInput = prompt("Введіть будь-які дані:");
 
-        return;
-    }
+  if (userInput === null) {
+    alert("Ви скасували введення.");
 
-    alert(`Ви ввели дані ${DataInput} разів.`);
+    return;
+  }
 
-    DataInput = DataInput + 1;
+  alert(`Ви ввели дані ${DataInput} разів.`);
 
-    getUserInput();
+  DataInput = DataInput + 1;
+
+  getUserInput();
 }
 
 getUserInput();
-
 
 // 4 Завдання
 
@@ -52,17 +49,29 @@ const numbers = [1, 2, 3, 4, 5];
 function CallbackArray(array, callback) {
   let result = [];
   for (let i = 0; i < array.length; i++) {
-      result.push(callback(array[i]));
+    result.push(callback(array[i]));
   }
   return result;
 }
 
-const multiplyNumbers = CallbackArray(numbers, function(num) {
+const multiplyNumbers = CallbackArray(numbers, function (num) {
   return num * num;
 });
 
 console.log(multiplyNumbers);
 
-
 // 5 Завдання
 
+const calculateDiscountedPrice = (price, discountPercent) => {
+  const applyDiscount = (price, discount) => price - price * (discount / 100);
+
+  return applyDiscount(price, discountPercent);
+};
+
+const originalPrice = 100;
+const discount = 20;
+const discountedPrice = calculateDiscountedPrice(originalPrice, discount);
+
+console.log(`Оригінальна ціна: ${originalPrice}`);
+console.log(`Знижка: ${discount}%`);
+console.log(`Дисконтна ціна: ${discountedPrice}`);
